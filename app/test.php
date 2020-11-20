e2520898c915ae93775d62607829225aae8dcf9b
‘classicoman2’ <aamengual@cifpfbmoll.eu>
2020-11-20 18:25:14 +0100

    prova

--- app/test.php
+++ app/test.php
@@ -3,12 +3,12 @@
 // Show all information, defaults to INFO_ALL
 $x = "prova";
 
-forward_static_call
-
 phpinfo();
 
 // Show just the module information.
 // phpinfo(8) yields identical results.
-phpinfo(INFO_MODULES);
+
+echo "patata";
+
 
 ?>
