ca74ef1ba159ecb1b014a64e4406483abb3a59c2
Toni <aamengual@cifpfbmoll.eu>
2020-11-26 22:21:45 +0100

    api completa

--- app/api/index.php
+++ app/api/index.php
@@ -13,8 +13,6 @@ $conn = $database->getConnection();
 
 // ROUTER   xtoni --> hauria de comprovar el path amb expressions regulars !
 
-
-
 /**  Identificar quin tipus d'operació és */
 // https://restfulapi.net/rest-put-vs-post/
 
@@ -52,8 +50,64 @@ if (isset($_GET['client'])) {
   if ($_SERVER['REQUEST_METHOD'] == "DELETE") {
     echo deleteClient($conn, $id);
   }
+  if ($_SERVER['REQUEST_METHOD'] == "PUT") {
+    echo updateClient($conn, $id);
+  }
 }
 
+
+function updateClient($conn, $id)
+{
+  // required headers
+  header("Access-Control-Allow-Origin: *");
+  header("Content-Type: application/json; charset=UTF-8");
+  header("Access-Control-Allow-Methods: POST");
+  header("Access-Control-Max-Age: 3600");
+  header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+  // get posted data
+  $data = json_decode(file_get_contents("php://input"));
+
+  // make sure data is not empty
+  if (
+    !empty($data->name) &&
+    !empty($data->surname) &&
+    !empty($data->age)
+  ) {
+    $name = $data->name;
+    $surname = $data->surname;
+    $age = $data->age;
+
+    // prepare & execute query statement
+    $stmt = $conn->prepare("UPDATE `clients` SET `name`='$name',`surname`='$surname',`age`=$age WHERE `id`=$id");
+    $result = $stmt->execute();
+
+    if ($result) { // ha anat be
+      // set response code - 201 created
+      http_response_code(201);
+
+      // tell the user
+      echo json_encode(array("message" => "Client was updated."));
+    }
+
+    // if unable to create the product, tell the user
+    else {
+
+      // set response code - 503 service unavailable
+      http_response_code(503);
+
+      // tell the user
+      echo json_encode(array("message" => $stmt->errorInfo()));
+    }
+  } else {
+
+    // set response code - 400 bad request
+    http_response_code(400);
+
+    // tell the user
+    echo json_encode(array("message" => "Unable to update product. Data is incomplete."));
+  }
+}
 
 function deleteClient($conn, $id)
 {
@@ -69,8 +123,6 @@ function deleteClient($conn, $id)
   $result = $stmt->execute();
 
   if ($result) { // ha anat be
-
-
     // set response code - 200 ok
     http_response_code(200);
 
@@ -80,7 +132,6 @@ function deleteClient($conn, $id)
 
   // if unable to delete the product
   else {
-
     // set response code - 503 service unavailable
     http_response_code(503);
 
