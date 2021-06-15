f8dc9a8b2a487a7999bd487abba7dd1743cb1984
‘classicoman2’ <aamengual@cifpfbmoll.eu>
2021-06-15 12:59:04 +0200

    sql: file updated

--- app/sql/api_rest_php.sql
+++ app/sql/api_rest_php.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Temps de generació: 07-12-2020 a les 13:59:48
+-- Temps de generació: 15-06-2021 a les 11:58:36
 -- Versió del servidor: 5.7.30-0ubuntu0.18.04.1-log
 -- Versió de PHP: 7.4.8
 
@@ -36,6 +36,14 @@ CREATE TABLE `cyclists` (
   `fk_team` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Bolcament de dades per a la taula `cyclists`
+--
+
+INSERT INTO `cyclists` (`id`, `name`, `birth_date`, `height`, `weight`, `fk_team`) VALUES
+(1, 'Peter Froome', '2077-06-15', 189, 76, 1),
+(2, 'Toni Dumolen', '2077-06-16', 190, 72, 2);
+
 -- --------------------------------------------------------
 
 --
@@ -59,6 +67,14 @@ CREATE TABLE `teams` (
   `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Bolcament de dades per a la taula `teams`
+--
+
+INSERT INTO `teams` (`id`, `name`) VALUES
+(1, 'Sky'),
+(2, 'Jumbo');
 
 --
 -- Índexs per a les taules bolcades
@@ -90,7 +106,7 @@ ALTER TABLE `teams`
 -- AUTO_INCREMENT per la taula `cyclists`
 --
 ALTER TABLE `cyclists`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT per la taula `cyclist_team_year`
@@ -102,7 +118,7 @@ ALTER TABLE `cyclist_team_year`
 -- AUTO_INCREMENT per la taula `teams`
 --
 ALTER TABLE `teams`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
